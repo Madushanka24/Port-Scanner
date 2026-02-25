f47a6b871bf436e66070d5ada976ae27f7ad19f1
Dillow <madushanka20021224@gmail.com>
2026-02-25 13:01:45 +0530

    multithreding add

--- port_scanner.py
+++ port_scanner.py
@@ -1,30 +1,23 @@
 import socket
+import threading
 
-# Ask user for target
 target = input("Enter target IP address: ")
-
-# Ask for port range
 start = int(input("Start port: "))
 end = int(input("End port: "))
 
 print(f"\nScanning {target} from port {start} to {end}...\n")
 
-# Loop through port range
-for port in range(start, end + 1):
-
-    # Create socket
+def scan(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Set timeout (in seconds)
     s.settimeout(0.5)
 
-    # Try connecting
-    result = s.connect_ex((target, port))
-
-    if result == 0:
+    if s.connect_ex((target, port)) == 0:
         print(f"Port {port} is OPEN")
 
-    # Close connection
     s.close()
 
-print("\nScan complete.")
+for port in range(start, end + 1):
+    thread = threading.Thread(target=scan, args=(port,))
+    thread.start()
+
+print("Scan started...\n")
